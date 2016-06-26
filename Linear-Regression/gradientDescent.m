b182746d1b3ece75416c1993e710563669e1474c
deepsidhpura777 <deepsidhpura777@gmail.com>
2016-06-26 19:17:39 +0530

    Add files via upload

---
+++ Linear-Regression/gradientDescent.m
@@ -0,0 +1,41 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+n=size(X,2);
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+h=X * theta;
+h_v= h * ones(1,n);
+y_v= y * ones(1,n);
+
+
+theta=theta - alpha * (1/m) * sum( (h_v - y_v) .* X )' ;
+
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+
+
+
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
